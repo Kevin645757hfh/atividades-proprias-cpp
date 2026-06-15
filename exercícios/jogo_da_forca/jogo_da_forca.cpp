efce73de361e0cf1465da1755f19e3a174e1ada9
愛 . <k6027939@gmail.com>
2026-06-15 16:36:53 -0300

    Update jogo_da_forca.cpp

--- exercícios/jogo_da_forca/jogo_da_forca.cpp
+++ exercícios/jogo_da_forca/jogo_da_forca.cpp
@@ -6,18 +6,18 @@
 using std::cout;
 using std::cin;
 using std::string;
-
+using std::endl;
 int main() {
 
     string palpite[4];
     
     string dica1[3] {"É um equipamento eletrônico.", "Pode ser usado para programar e jogar.", "Possui teclado e monitor."}, dica2[3] = {"É um animal terrestre.","Possui uma tromba longa.", "É um dos maiores mamíferos do mundo."}, dica3[3] = {"É um esporte muito popular no Brasil", "É jogado com uma bola.", "Cada equipe normalmente tem 11 jogadores"}, dica4[3] = {"É um alimento doce.", "É feito a partir do cacau.", "Muitas pessoas ganham isso na Páscoa."}, dica5[3] = {"Trabalha em missões espaciais.", "Usa uma roupa especial.", "Pode viajar para fora da Terra."}, dica6[3] = {"É um local de estudo", "Contém muitos livros.", "Geralmente exige silêncio."};
     
-    string palavras[6] = {"computador", "elefante", "futebol", "chocolate", "astronauta", "biblioteca"};
+    string palavra;
     
     char letra;
     bool palavraDescoberta = false;
-    int num;
+    int num = 0;
 
     cout << "deseja começar a jogar o jogo da forca? s/n: ";
     cin >> letra;
@@ -26,7 +26,7 @@ int main() {
         
         cout << "Inicinado o jogo..." << std::endl;
         
-        std::this_thread::sleep_for(std::chrono::seconds(3));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
         
         cout << "\nJogo iniciado\n";
     }
@@ -42,5 +42,24 @@ int main() {
     cout << "Bem vindo ao jogo da forca escolha um número de 1 a 6 para sortearmos sua palavras: ";
     cin >> num;
     
+    if(num == 1){
+        palavra = "computador";
+    }
+    else if (num == 2){
+        palavra = "elefante";
+    }
+    else if(num == 3){
+        palavra = "futebol";
+    }
+    else if(num == 4){
+        palavra = "chocolate";
+    }
+    else if(num == 5){
+        palavra = "astronauta";
+    }
+    else {
+        palavra = "biblioteca";
+    }
+    cout << "p:" << palavra << endl;
     return 0;
 }
